0789e174e80dd3ce7dfc510058ee0594c175db26
Mir-Labib-Hossain <mirlabibhossain99@gmail.com>
2022-10-06 13:33:29 +0600

    Implemented webpack with lodash

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const bundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "awsome.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [new bundleAnalyzerPlugin()],
+ 
+};
