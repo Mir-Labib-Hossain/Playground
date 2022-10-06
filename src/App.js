17d46e7abf49e39faf1d0a463a3b9791ac8446be
Mir-Labib-Hossain <mirlabibhossain99@gmail.com>
2022-10-06 16:43:27 +0600

    Created react fron scratch

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import "./App.css"
+import Nav from './components/nav'
+
+const App = () => {
+  return (
+    <>
+    <Nav/>
+    <div>Hello World</div>
+    <img src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg" alt="" />
+    </>
+  )
+}
+
+export default App
