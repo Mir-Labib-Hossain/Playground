2bf4ea4f17b93fcd37229330e679b0031502c076
Mir-Labib-Hossain <mirlabibhossain99@gmail.com>
2022-12-02 16:25:15 +0600

    Spline added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import SplineComp from "./spline-comp";
+
 type Props = {};
 
 const App = (props: Props) => {
-  return <div>App</div>;
+  return (
+    <div>
+      <SplineComp />
+      <h1>Hello</h1>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/spline-comp/index.tsx
@@ -0,0 +1,10 @@
+type Props = {};
+import Spline from "@splinetool/react-spline";
+const SplineComp = (props: Props) => {
+  
+  return <iframe src='https://my.spline.design/wizardroomcopy-6d241a2077e9417ecff64fc9f9995dcd/' frameBorder='0' width='100%' height='1000px'></iframe>
+  // return <Spline scene="https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode" />
+
+};
+
+export default SplineComp;
