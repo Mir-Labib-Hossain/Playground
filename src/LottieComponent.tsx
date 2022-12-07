5f32c77766b5f4161df53589ad945341486e8852
Mir-Labib-Hossain <mirlabibhossain99@gmail.com>
2022-12-07 10:40:58 +0600

    IDK

--- src/LottieComponent.tsx
+++ src/LottieComponent.tsx
@@ -1,14 +1,40 @@
-import { useState } from "react";
+// import { useState } from "react";
+// // import Lottie from "react-lottie";
 // import Lottie from "react-lottie";
+// import * as animationData from "./assets/lottie.json";
+
+// const LottieComponent = () => {
+//   const [isStopped, setIsStopped] = useState(false);
+//   const [isPaused, setIsPaused] = useState(false);
+//   console.log("LottieComponent");
+
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: animationData,
+//     rendererSettings: {
+//       preserveAspectRatio: "xMidYMid slice",
+//     },
+//   };
+
+//   return (
+//     <div>
+//       <Lottie options={defaultOptions} height={400} width={400} isStopped={isStopped} isPaused={isPaused} />
+//       <button onClick={() => setIsStopped(true)}>stop</button>
+//       <button onClick={() => setIsStopped(false)}>play</button>
+//       <button onClick={() => setIsPaused(!isPaused)}>pause</button>
+//     </div>
+//   );
+// };
+
+// export default LottieComponent;
+
 import Lottie from "react-lottie";
-import * as animationData from "./assets/lottie.json";
+import animationData from "./assets/lottie.json";
+type Props = {};
 
-const LottieComponent = () => {
-  const [isStopped, setIsStopped] = useState(false);
-  const [isPaused, setIsPaused] = useState(false);
-  console.log("LottieComponent");
-
-  const defaultOptions = {
+const LottieComponent = (props: Props) => {
+    const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: animationData,
@@ -16,15 +42,7 @@ const LottieComponent = () => {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
-
-  return (
-    <div>
-      <Lottie options={defaultOptions} height={400} width={400} isStopped={isStopped} isPaused={isPaused} />
-      <button onClick={() => setIsStopped(true)}>stop</button>
-      <button onClick={() => setIsStopped(false)}>play</button>
-      <button onClick={() => setIsPaused(!isPaused)}>pause</button>
-    </div>
-  );
+  return <Lottie options={defaultOptions}/>;
 };
 
 export default LottieComponent;
